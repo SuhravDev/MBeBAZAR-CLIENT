927f72b3a0eeb1ea6f229e7d038244fd3bdfb45c
SuhravHussen <suhravshan@gmail.com>
2022-09-14 13:10:57 +0600

    bug fix

--- utils/handleLogout.ts
+++ utils/handleLogout.ts
@@ -1,22 +1,27 @@
-import { useRouter } from 'next/router';
 import { signOut } from 'next-auth/react';
+
 const logeOut = async (): Promise<boolean> => {
-  try {
-    const resData = await fetch(`${process.env.BASE_URL}/auth/logout`, {
-      method: 'GET',
-      credentials: 'include',
-    }).then(res => res.json());
-    if (resData.error) {
-      return false;
-    } else {
-      localStorage.removeItem('user');
-      localStorage.removeItem('session');
-      signOut();
-      return true;
+    try {
+        const resData = await fetch(`${process.env.BASE_URL}/auth/logout`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        }).then((res) => res.json());
+        if (resData.error) {
+            return false;
+        }
+        localStorage.removeItem('user');
+        localStorage.removeItem('session');
+        const loggedOut = await signOut();
+        if (loggedOut) {
+            return true;
+        }
+        return false;
+    } catch {
+        return false;
     }
-  } catch {
-    return false;
-  }
 };
 
 export default logeOut;
